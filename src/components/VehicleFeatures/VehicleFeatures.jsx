e25297f96c1df9334310bd2073f592a936dade1b
Roman Iakymovich <romaniakymovich@MacBook-Pro-Roman.local>
2024-10-27 23:18:34 +0200

    +

--- src/components/VehicleFeatures/VehicleFeatures.jsx
+++ src/components/VehicleFeatures/VehicleFeatures.jsx
@@ -1,20 +1,26 @@
+import PropTypes from 'prop-types';
 import css from "./VehicleFeatures.module.css";
 import { useSelector } from "react-redux";
 import { selectCamperDetails } from "../../redux/selectors";
 import VehicleAttributeList from "../VehicleAttributeList/VehicleAttributeList";
 
+const VEHICLE_FORMS = {
+  fullyIntegrated: "Fully Integrated",
+  alcove: "Alcove",
+};
+
 function VehicleFeatures() {
   const details = useSelector(selectCamperDetails);
-  const vehicleFormFormatter = (form) => {
-    switch (form) {
-      case "fullyIntegrated":
-        return "Fully Integrated";
-      case "alcove":
-        return "Alcove";
-      default:
-        return form;
-  }
-  }
+  const { form, length, width, height, tank, consumption } = details;
+
+  const features = [
+    { title: "Form", value: VEHICLE_FORMS[form] || form },
+    { title: "Length", value: length },
+    { title: "Width", value: width },
+    { title: "Height", value: height },
+    { title: "Tank", value: tank },
+    { title: "Consumption", value: consumption },
+  ];
 
   return (
     <div className={css.container}>
@@ -24,33 +30,26 @@ function VehicleFeatures() {
       <h2 className={css.title}>Vehicle details</h2>
       <hr className={css.underline}/>
       <ul className={css.vehicleFeatures}>
-        <li className={css.vehicleFeature}>
-          <p className={css.featureTitle}>Form</p>
-          <p className={css.featureValue}>{vehicleFormFormatter(details.form)}</p>
-        </li>
-        <li className={css.vehicleFeature}>
-          <p className={css.featureTitle}>Length</p>
-          <p className={css.featureValue}>{details.length}</p>
-        </li>
-        <li className={css.vehicleFeature}>
-          <p className={css.featureTitle}>Width</p>
-          <p className={css.featureValue}>{details.width}</p>
-        </li>
-        <li className={css.vehicleFeature}>
-          <p className={css.featureTitle}>Height</p>
-          <p className={css.featureValue}>{details.height}</p>
-        </li>
-        <li className={css.vehicleFeature}>
-          <p className={css.featureTitle}>Tank</p>
-          <p className={css.featureValue}>{details.tank}</p>
-        </li>
-        <li className={css.vehicleFeature}>
-          <p className={css.featureTitle}>Consumption</p>
-          <p className={css.featureValue}>{details.consumption}</p>
-        </li>
+        {features.map(({ title, value }) => (
+          <li key={title} className={css.vehicleFeature}>
+            <p className={css.featureTitle}>{title}</p>
+            <p className={css.featureValue}>{value}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
 }
+
+VehicleFeatures.propTypes = {
+  details: PropTypes.shape({
+    form: PropTypes.string,
+    length: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    tank: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    consumption: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  }),
+};
 
 export default VehicleFeatures;
